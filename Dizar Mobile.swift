4e827d72de62ed015042a3ffe546a54890647d19
Luciana478 <118472754+Luciana478@users.noreply.github.com>
2022-11-19 17:43:59 -0300

    Add files via upload

---
+++ Dizar Mobile.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var setup = "Welcome!"
+var textElement = "Create, comment, and manage AIRS with Dizar Mobile!"
+
+var button = "Let's Go!"
+var otherButton = "Continue without Login"
+
+func setupEngine(Email, Password) {
+
+} {
+    var otherTextElement = "Let's connect to your Dizar Profile"
+    var otherTextElementChild = "If you don't have any Profile, click HERE to sign up!"
+}
