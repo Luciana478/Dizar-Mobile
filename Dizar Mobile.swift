7abd7650768bd5d7814ec3b958d42c5721ee1bed
Luciana478 <118472754+Luciana478@users.noreply.github.com>
2022-11-19 17:49:12 -0300

    File version 1.2

--- Dizar Mobile.swift
+++ Dizar Mobile.swift
@@ -12,3 +12,4 @@ func setupEngine(Email, Password) {
     var otherTextElement = "Let's connect to your Dizar Profile"
     var otherTextElementChild = "If you don't have any Profile, click HERE to sign up!"
 }
+
