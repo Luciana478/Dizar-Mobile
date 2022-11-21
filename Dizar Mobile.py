a144f1398a5e64af1d11ef0ea87ec069a57a1ba1
Rafael Canarei <118677397+RafaelCanarei5@users.noreply.github.com>
2022-11-21 12:24:40 -0300

    Update Dizar Mobile.py

--- Dizar Mobile.py
+++ Dizar Mobile.py
@@ -1,15 +1,1 @@
-import UIKit
-
-var setup = "Welcome!"
-var textElement = "Create, comment, and manage AIRS with Dizar Mobile!"
-
-var button = "Let's Go!"
-var otherButton = "Not now"
-
-func setupEngine() {
-
-} {
-    var otherTextElement = "Let's connect to your Dizar Profile"
-    var otherTextElementChild = "If you don't have any Profile, click HERE to sign up!"
-}
 
