01b407ad7385dfa3c659ef3f3df61ff83b16f443
ThiagoPimentel11 <118989343+ThiagoPimentel11@users.noreply.github.com>
2022-11-25 11:18:35 -0300

    Update and rename app.swift to app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import PyUI23
+
+def greetings(self):
+    layout = [
+        self.firstHeadingLabel("Welcome back!"),
+        self.secondHeadingLabel
+
+
+
+    ]
+        
--- app.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-func greetings() {
-    var str = "Welcome!"
-    var str2 = "Sign in with your Account to continue."
-    var button = "Let's go!"
-    var otherButton = "Later"
-}
