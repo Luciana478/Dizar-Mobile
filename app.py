6ce6c0c864e52cec361cfb302aa69eb58556037b
EduardoCanarei8 <119135545+EduardoCanarei8@users.noreply.github.com>
2022-11-26 17:55:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ def greetings(self):
     layout = [
         self.firstHeadingLabel("Welcome back!"),
         self.paragraph("Sign In to your account to continue"),
-
-
-
+        self.Button("Let's go") and self.Button("Later")
     ]
         
